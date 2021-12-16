34253d81f58859095f374903d412d7fa41d0ebe5
Im-Haru <lucasleguennec24@gmail.com>
2021-12-16 17:05:32 +0100

    Base du projet Game V2
    chuis un bg du turfu

--- GameV2.py/Attaques.py
+++ GameV2.py/Attaques.py
@@ -1,6 +1,11 @@
 import sys
 
+class Attaque:
 
+    def __init__(self, nom = "Attaque de base", degats = 10, mana = 5):
+        self.nom = nom
+        self.degats = degats
+        self.mana = mana
 
 
 
---
+++ GameV2.py/Personnages.py
@@ -0,0 +1,39 @@
+class Personnage:
+
+    def __init__(self, nom = "", pv = 100, mana = 20, critiques = 1):
+        self.nom = nom
+        self.pv = pv
+        self.mana = mana
+        self.critiques = critiques
+
+    def Presentation(self):
+        print("Je suis un personnage", self.nom)
+
+    def PrendreAttaque(self, attaque):
+        self.pv -= attaque.degats
+        print(self.nom, "perds", attaque.degats, "PVs")
+
+    def Attaquer(self, attaque, ennemi):
+        print(self.nom, "utilise l'attaque", attaque.nom)
+        self.mana -= attaque.mana
+        ennemi.PrendreAttaque(attaque)
+
+class Mage(Personnage):
+    def __init__(self, nom):
+        self.nom = nom
+        self.pv = 80
+        self.mana = 150
+        self.critiques = 5
+
+    def Presentation(self):
+        print("Vous ne passerez pas !")
+
+class Guerrier(Personnage):
+    def __init__(self, nom):
+        self.nom = nom
+        self.pv = 300
+        self.mana = 10
+        self.critiques = 0.5
+
+    def Presentation(self):
+        print("GRRRRR !")
